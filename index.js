8c85a31d5b05dc32354b046d867ec9e8ba667d86
isaiful508 <isaiful.islam508@gmail.com>
2024-06-02 10:38:38 +0600

    check email admin or not

--- index.js
+++ index.js
@@ -35,6 +35,23 @@ async function run() {
 
 
 
+    //middlewares
+
+    // const verifyAdmin = async (req, res, next) => {
+    //   const email = req.decoded.email
+    //   const query = { email: email }
+    //   const user = await userCollection.findOne(query);
+    //   const isAdmin = user?.role === 'admin';
+    //   if (!isAdmin) {
+    //     return res.status(403).send({ message: 'forbidden access' })
+    //   }
+    //   next()
+    // }
+
+
+
+
+
     app.post('/users', async (req, res) => {
       try {
         const user = req.body;
@@ -71,10 +88,35 @@ async function run() {
     }
   }
 
+
   const result = await userCollection.updateOne(filter, updatedDoc);
   res.send(result);
 
 })
+
+//search admin by email
+app.get('/users/admin/:email', async (req, res) => {
+  const email = req.params.email;
+
+  // if (email !== req.decoded.email) {
+  //   return res.status(403).send({ message: 'forbidden access' })
+  // }
+
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  let admin = false;
+  if (user) {
+    admin = user?.role === 'admin';
+
+  }
+  res.send({ admin })
+
+})
+
+
+
+
+
 
 
 
